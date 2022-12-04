6f2aa91e1a401c6bd81c7577de277407c2649c04
Davin Tryon <davin@lockdown-2.local>
2022-12-04 08:48:49 +0000

    day4

---
+++ src/day4.clj
@@ -0,0 +1,36 @@
+(ns day4
+  (:require
+    [clojure.string :as str]
+    [utils :refer [read-file]]))
+
+(defn has-overlap? [[a b] [x y]]
+  (cond (and (>= a x) (<= b y)) 1
+        (and (>= x a) (<= y b)) 1
+        :else 0))
+
+(defn first-star []
+  (->> (read-file "src/day4.input")
+       (map #(str/split % #","))
+       (map #(map (fn [xs] (map (fn [n] (Integer/parseInt n)) (str/split xs #"-"))) %))
+       (map #(has-overlap? (first %) (second %)))
+       (apply +)))
+
+(comment
+  (first-star))
+
+(defn has-overlap2? [[a b] [x y]]
+  (cond (and (>= a x) (<= b y)) 1
+        (and (>= x a) (<= y b)) 1
+        (and (<= a x) (>= b x)) 1
+        (and (<= a y) (>= b y)) 1
+        :else 0))
+
+(defn second-star []
+  (->> (read-file "src/day4.input")
+       (map #(str/split % #","))
+       (map #(map (fn [xs] (map (fn [n] (Integer/parseInt n)) (str/split xs #"-"))) %))
+       (map #(has-overlap2? (first %) (second %)))
+       (apply +)))
+
+(comment
+  (second-star))
---
+++ src/day5.clj
@@ -0,0 +1,15 @@
+(ns day5
+  (:require [utils :refer [read-file]]))
+
+
+(defn first-star []
+  (->> (read-file "src/day5.input")))
+
+(comment
+  (first-star))
+
+(defn second-star []
+  (->> (read-file "src/day5.input")))
+
+(comment
+  (second-star))
