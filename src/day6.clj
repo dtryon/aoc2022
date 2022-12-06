9073140f0d798333bbece23367db35fa93deccdc
Davin Tryon <davin@lockdown-2.local>
2022-12-06 08:10:39 +0000

    day6

--- src/day6.clj
+++ src/day6.clj
@@ -1,16 +1,25 @@
 (ns day6
   (:require
-    ;[clojure.string :as str]
     [utils :refer [read-file]]))
 
 (defn first-star []
-  (->> (read-file "src/day6.test.input")))
+  (->> (read-file "src/day6.input")
+       (first)
+       (partition 4 1)
+       (keep-indexed #(if (= (count (set %2)) 4) %1 nil))
+       (first)
+       (+ 4)))
 
 (comment
   (first-star))
 
 (defn second-star []
-  (->> (read-file "src/day6.input")))
+  (->> (read-file "src/day6.input")
+       (first)
+       (partition 14 1)
+       (keep-indexed #(if (= (count (set %2)) 14) %1 nil))
+       (first)
+       (+ 14)))
 
 (comment
   (second-star))
---
+++ src/day7.clj
@@ -0,0 +1,14 @@
+(ns day7
+  (:require [utils :refer [read-file]]))
+
+(defn first-star []
+  (->> (read-file "src/day7.input")))
+
+(comment
+  (first-star))
+
+(defn second-star []
+  (->> (read-file "src/day7.input")))
+
+(comment
+  (second-star))
