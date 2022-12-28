6558ac629c6fa7997080915a98a0dbfdbcbf8bc1
Davin Tryon <davin@lockdown-2.local>
2022-12-28 12:43:17 +0000

    day7

--- src/day8.clj
+++ src/day8.clj
@@ -7,8 +7,6 @@
 (defn count-trees [coll]
   (reduce (fn [acc row]
             (+ acc (:total (reduce (fn [tree-acc tree]
-                                     ; (println (str "tree " tree))
-                                     ;(println (str "counted " @counted))
                                      (if (> (:value tree) (:max tree-acc))
                                        (let [next-max (:value tree)
                                              total (:total tree-acc)]
@@ -44,8 +42,47 @@
 (comment
   (first-star))
 
+(defn search-right [grid tree x y]
+  (let [new-y (inc y) v (get-in grid [x new-y])]
+    (cond (nil? v) 0
+          (>= v tree) 1
+          :else (+ 1 (search-right grid tree x new-y)))))
+
+(defn search-left [grid tree x y]
+  (let [new-y (dec y) v (get-in grid [x new-y])]
+    (cond (nil? v) 0
+          (>= v tree) 1
+          :else (+ 1 (search-left grid tree x new-y)))))
+
+(defn search-up [grid tree x y]
+  (let [new-x (inc x) v (get-in grid [new-x y])]
+    (cond (nil? v) 0
+          (>= v tree) 1
+          :else (+ 1 (search-up grid tree new-x y)))))
+
+(defn search-down [grid tree x y]
+  (let [new-x (dec x) v (get-in grid [new-x y])]
+    (cond (nil? v) 0
+          (>= v tree) 1
+          :else (+ 1 (search-down grid tree new-x y)))))
+
+(defn scenic-score [grid]
+  (map-indexed
+    #(map-indexed (fn [idx tree]
+                    (let [x %1 y idx]
+                      (* (search-right grid tree x y)
+                         (search-left grid tree x y)
+                         (search-up grid tree x y)
+                         (search-down grid tree x y)))) %2) grid))
+
 (defn second-star []
-  (->> (read-file "src/day8.input")))
+  (->> (read-file "src/day8.input")
+       (map #(map (fn [c] (Integer/parseInt (str c))) %))
+       (map vec)
+       vec
+       scenic-score
+       flatten
+       (apply max)))
 
 (comment
   (second-star))
